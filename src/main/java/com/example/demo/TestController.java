a5257fc9116dd619a9517bb566f08b2c708cd5bc
shinebyul <qufdl8382@gmail.com>
2024-08-09 14:36:12 +0900

    [Init]초기파일

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public void test(){
+        System.out.println("8월 9일 DevOps 수업");
+    }
+
+}
